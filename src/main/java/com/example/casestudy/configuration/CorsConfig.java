d0ca5786cb23740f85cee505fcf337ddd099bcb9
pnikolidakis <pnikolidakis@procureship.com>
2023-09-17 15:07:37 +0300

    remove comments

--- src/main/java/com/example/casestudy/configuration/CorsConfig.java
+++ src/main/java/com/example/casestudy/configuration/CorsConfig.java
@@ -11,10 +11,10 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:4200") // Whitelist specific origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
-                .allowCredentials(true) // Allow credentials like cookies
-                .maxAge(3600); // Max age of the pre-flight request (1 hour in seconds)
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
